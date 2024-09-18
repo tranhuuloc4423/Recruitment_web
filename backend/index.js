bd91d99a5b0a9d10513d18006169a0e77a828404
VanTuanDev <vantuanmhung2001@gmail.com>
2024-09-18 21:31:00 +0700

    add cors package

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const cors = require('cors')
 
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 const MONGODB_URI = process.env.MONGODB_URI
 const appRoute = require('./routes/index')
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000'
+  })
+)
 
 app.use(express.json())
 app.use('/', appRoute)
